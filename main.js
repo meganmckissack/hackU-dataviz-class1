946fdb46363c3d8add9e24d2d229141b6705d6b2
Megan McKissack <meganmckissack@gmail.com>
2016-03-21 16:26:41 -0700

    Added files via upload

--- main.js
+++ main.js
@@ -40,50 +40,91 @@ var data = [{
   "nature": "plus good plus "
 }]
 
+/*var dataSet = _.map(_.range(40), function(i) {
+  return {
+    x: Math.random() *100,
+    y: Math.random() *100,
+    r: Math.random() *30
+  };
+});*/
+
+var dataSet = data.map(function(val){
+  return {
+    x: Math.random() *100,
+    y: Math.random() *100,
+    r: val.nature.length*2
+  }
+});
 
 
-var newData=[];
-for (i = 0; i < data.length; i++) {
-  var item = data[i]["nature"].length;
-  newData.push(item);
-}
 
-var height = 200;
-var width = 400;
+var margin = {
+  top: 20,
+  right: 20,
+  bottom: 60,
+  left: 40
+};
+var height = 400 - margin.top - margin.bottom;
+var width = 600 - margin.left - margin.right;
 
 var yScale = d3.scale.linear()
-  .domain([0, d3.max(newData)*1.1])
-  .range([0, height]);
+  .domain([0, d3.max(dataSet, function(data) {
+    return data.y
+  })])
+  .range([height, 0]);
 
-var xScale = d3.scale.ordinal()
-    .domain(newData)
-    .rangeBands([0, width], 0.25, 0.25);
+var xScale = d3.scale.linear()
+  .domain([0, 100])
+  .range([0, width]);
 
-var colorScale = d3.scale.linear()
-    .domain([0,d3.max(newData)]) //data based
-    //.domain([0,newData.length]) //position based
-    .range(['#55C3DC', '#73726D', '#E24B2C']);
+/*var colorScale = d3.scale.linear()
+  .domain([0, d3.max(dataSet)]) //data based
+  //.domain([0,newData.length]) //position based
+  .range(['#55C3DC', '#73726D', '#E24B2C']);*/
 
-var svg = d3.select('#barChart').append('svg')
-    .attr('width', width)
-    .attr('height', height);
+var svg = d3.select('#chartArea').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-svg.selectAll('rect')
-    .data(newData)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x', function(data, index){
-      return xScale(data)
-    })
-    .attr('y', function(data) {
-      return height - yScale(data)
-    })
-    .attr('width', xScale.rangeBand)
-    .style('height', function (data) {
-      return yScale(data)
-    })
-    .attr('fill', function(data, i) {
-      return colorScale(data) //data based
-      //return colorScale(i) //position based
-    })
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient('bottom')
+    .ticks(10)
+    .innerTickSize(10)
+    .outerTickSize(10)
+    .tickPadding(10);
+
+svg.append('g')
+  .attr('class', 'x axis')
+  .attr('transform', 'translate(0, '+ (height + 10) + ')')
+  .call(xAxis);
+
+var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient('left')
+  .ticks(5)
+  .innerTickSize(10)
+  .outerTickSize(2)
+  .tickPadding(10);
+
+svg.append('g')
+  .attr('class', 'y axis')
+  .attr('transform', 'translate(0, '+ 10 + ')')
+  .call(yAxis);
+
+svg.selectAll('circle')
+  .data(dataSet)
+  .enter()
+  .append('circle')
+  .attr('class', 'scatter')
+  .attr('cx', function(data) {
+    return xScale(data.x)
+  })
+  .attr('cy', function(data) {
+    return yScale(data.y)
+  })
+  .attr('r', function(data) {
+    return data.r
+  });
